49cabb5b56a9f97625727edfa1c9313ff45c8286
Clayton Persinger <65939048+oMnotopia@users.noreply.github.com>
2021-10-04 14:16:36 -0700

    changed onclick event that grows/shrinks blog items from in line html to script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 //onclick called from inline html, enlarges blog item when clicked and shrinks when clicked again
-const scopeId = document.getElementById('scope');
-//const hoistingId = document.getElementById('hoisting')
+const hoistingId = document.getElementById('hoisting')
 
-scopeId.onclick = enlargeBlogItem(this)
-//hoistingId.onclick = enlargeBlogItem(this)
+hoistingId.onclick = enlargeBlogItem
 
-function enlargeBlogItem(el) {
-    const origSize = el.getAttribute('attr-small')
-    if(el.style.height){
-        el.style.removeProperty('height');
+function enlargeBlogItem() {
+    const origSize = hoistingId.attributes['attr-small'].value
+    if(hoistingId.style.height){
+        hoistingId.style.removeProperty('height');
     } else {
-        el.style.height = origSize;
+        hoistingId.style.height = origSize;
     }
 }
 
