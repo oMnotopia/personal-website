6e6e179a9db4bea88eee373558083885e151cffe
Clayton Persinger <65939048+oMnotopia@users.noreply.github.com>
2022-01-26 15:28:07 -0800

    overhaul the dark mode slider bars look and functionality

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,7 @@
-//Changes between dark mode and light mode, using the slider bar in the header.
-//Global variables
-const sliderMode = document.getElementById('slider');
+
+
+const switchInput = document.getElementById('switch-input')
+
 const theme = document.getElementById('theme')
 const githubLogo = document.getElementsByClassName('icon')
 
@@ -13,11 +14,6 @@ const greenC = document.getElementById('green')
 const pinkC = document.getElementById('pink')
 const blueC = document.getElementById('blue')
 
-//Looks at if the user has dark mode set as default for their browser.
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-//Gets theme stored in local storage if the user has set a preference before.
-const currentTheme = localStorage.getItem("theme");
-
 //Contrast values for "Accessability" blog.
 const darkModeContrastValues = () => {
     yellowC.innerHTML = "13.91:1"
@@ -25,14 +21,33 @@ const darkModeContrastValues = () => {
     pinkC.innerHTML = "6.95:1"
     blueC.innerHTML = "1.73:1"
 }
-
-if(prefersDarkScheme) {
-    theme.className = "dark";
-    githubLogo[0].src = "./resources/github-dark-logo.png"; //logo in header 
-    if(githubLogo[1]) githubLogo[1].src = "./resources/github-dark-logo.png"; // logo in projects section
-    if(yellowC||greenC||pinkC||blueC) darkModeContrastValues()
+const handleMouseEnter = () => {
+    yellowC.innerHTML = "1.49:1"
+    greenC.innerHTML = "1.17:1"
+    pinkC.innerHTML = "1.33:1"
+    blueC.innerHTML = "5.35:1"
 }
-if (currentTheme === "dark") {
+const handleMouseLeave = () => {
+    yellowC.innerHTML = "1.07:1"
+    greenC.innerHTML = "1.37:1"
+    pinkC.innerHTML = "3.18:1"
+    blueC.innerHTML = "8.59:1"
+}
+
+const currentTheme = localStorage.getItem("theme")
+const rememberedTheme = localStorage.getItem("sliderChecked")
+
+//If user has dark mode set as preference
+// if(prefersDarkScheme) {
+//     theme.className = "dark";
+//     githubLogo[0].src = "./resources/github-dark-logo.png"; //logo in header 
+//     if(githubLogo[1]) githubLogo[1].src = "./resources/github-dark-logo.png"; // logo in projects section
+//     if(yellowC||greenC||pinkC||blueC) darkModeContrastValues()
+// }
+//Remeber status of theme from previous visits then update elements.
+
+if (rememberedTheme==='true') {
+    switchInput.checked = true; //Reapplys checked status of slider on page reload
     theme.className = "dark";
     githubLogo[0].src = "./resources/github-dark-logo.png"; //logo in header 
     if(githubLogo[1]) githubLogo[1].src = "./resources/github-dark-logo.png"; // logo in projects section
@@ -41,13 +56,17 @@ if (currentTheme === "dark") {
     theme.className = "light";
     githubLogo[0].src = "./resources/github-logo.png"; //logo in header 
     if(githubLogo[1]) githubLogo[1].src = "./resources/github-logo.png"; // logo in projects section  
+    if(yellowC||greenC||pinkC||blueC) handleMouseLeave()
 }
 
+console.log(rememberedTheme)
+console.log(theme.className)
 //Event listener from click. Event handler looks at the class name and changes between dark and light.
-sliderMode.addEventListener('click', () => {
+switchInput.addEventListener('click', () => {
 
     if(theme.className==='light'){
         theme.className = 'dark';
+
         githubLogo[0].src = "./resources/github-dark-logo.png"; //logo in header
         if(githubLogo[1]) githubLogo[1].src = "./resources/github-dark-logo.png"; // logo in projects section
         
@@ -64,6 +83,7 @@ sliderMode.addEventListener('click', () => {
 
     } else {
         theme.className = 'light';
+
         githubLogo[0].src = "./resources/github-logo.png";
         if (githubLogo[1]) githubLogo[1].src = "./resources/github-logo.png";
 
@@ -76,26 +96,12 @@ sliderMode.addEventListener('click', () => {
             accessabillity.addEventListener('mouseleave', handleMouseLeave)    
         }
     }
-
+    console.log(theme.className)
     //Stores theme in local storage so on a page switch theme is remembered.
     localStorage.setItem("theme", theme.className);
+    localStorage.setItem("sliderChecked", switchInput.checked)
 
 })
-
-
-
-const handleMouseEnter = () => {
-    yellowC.innerHTML = "1.49:1"
-    greenC.innerHTML = "1.17:1"
-    pinkC.innerHTML = "1.33:1"
-    blueC.innerHTML = "5.35:1"
-}
-const handleMouseLeave = () => {
-    yellowC.innerHTML = "1.07:1"
-    greenC.innerHTML = "1.37:1"
-    pinkC.innerHTML = "3.18:1"
-    blueC.innerHTML = "8.59:1"
-}
 
 //Event listeners that change contrast ratio values for accessability blog.
 if(accessabillity){
@@ -103,14 +109,4 @@ if(accessabillity){
     accessabillity.addEventListener('mouseleave', handleMouseLeave)   
 }
 
-// const btn = document.querySelector(".btn-toggle");
-// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-// btn.addEventListener("click", function () {
-//   if (prefersDarkScheme.matches) {
-//     document.body.classList.toggle("light-theme");
-//   } else {
-//     document.body.classList.toggle("dark-theme");
-//   }
-// });
 
